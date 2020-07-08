c5583b2b78aa6a87338f0734ce6ef66215692330
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-08 13:29:11 +0530

    Auth Modal Templates

--- Firebase-Functions/functions/index.js
+++ Firebase-Functions/functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// http rquest 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+    const number = Math.round(Math.random() * 100);
+    response.send(number.toString());
+})
+
+// http rquest 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+    response.redirect('https://www.thenetninja.co.uk');
+})
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+    const name = data.name;
+    return `hello, ${name}!! :) `;
+})
